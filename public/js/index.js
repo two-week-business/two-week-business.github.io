58277827a4ef0c398d5bdc500e3fec3946aaaad8
David Gaynor <david.gaynor@students.olin.edu>
2016-01-29 16:27:28 -0800

    added c name and made flippiesd work, and added logos and pics

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,31 @@
 function setContentHeight() {	
 	var viewportHeight = window.innerHeight;		
-	document.getElementById("section-1").style.height = viewportHeight;		
+	$("section-1").css("#height", viewportHeight);		
 
-	var sections = document.getElementsByClassName("section");
+	var sections = $(".section");
 	for (i = 0; i < sections.length; i ++)  {		
 		sections[i].style.visibility = "visible";
 	}
 }
 
-$(function() {	
+$(function() {		
+	var flipContainer = $(".flip-container")
+	flipContainer.click(function(e) {
+		$(e.delegateTarget).toggleClass("flip")		
+	})
+})
+
+$(window).load(function() {
 	setContentHeight();
+	
+	// now set the height for each of the flippers, which need don't have the correct height beacuse
+	// their children are absolutely positionned
+	console.log("LOADED!");
+	$(".flipper").each(function(i, el) {
+		$el = $(el)
+		var height = $el.find(".front").height();
+		var width = $el.find(".front").width();
+		$el.css("height", height);
+		$el.css("width", width);
+	})
 })
